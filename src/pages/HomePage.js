387cbe1c077c0a626186c4f8de6fe2f3b005e8e4
Georjane <pzgeorjane@gmail.com>
2024-04-26 05:20:08 +0100

    Style hero section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,14 +5,28 @@ import '../assets/css/Homepage.css';
 
 function HomePage() {
   return (
-    <div>
-      <h1 className="hero-h1">Transforming Breast Cancer Care, One Patient at a Time</h1>
-      <p className="hero-para">
+    <div className="hero-section py-5">
+      <h1 className="hero-h1 w-50 m-auto d-none d-md-block">
+        Transforming
+        <span className="hero-span"> Breast Cancer Care</span>
+        , One Patient at a Time
+      </h1>
+      <h1 className="hero-h1 m-auto px-4 d-block d-md-none">
+        Transforming
+        <span className="hero-span"> Breast Cancer Care</span>
+        , One Patient at a Time
+      </h1>
+      <p className="hero-para  w-50 m-auto py-3 px-5 d-none d-md-block">
         Check Me mobile app connects patients
         and doctors for seamless communication, effortless appointment management,
         and a more empowered healthcare experience.
       </p>
-      <div className="hero-btn">
+      <p className="hero-para  py-3 m-auto px-4 d-block d-md-none">
+        Check Me mobile app connects patients
+        and doctors for seamless communication, effortless appointment management,
+        and a more empowered healthcare experience.
+      </p>
+      <div className="hero-btn py-2">
         <Button type="default" text="Our Services" />
         <Button type="text-icon" text="Contact Us" icon={<FaAngleDown />} />
       </div>
